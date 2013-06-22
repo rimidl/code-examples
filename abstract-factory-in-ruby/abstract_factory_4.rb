27be7ec196ae1857e116643c4fa902a22617ad92
Vladimir Strakhov <vl.strakhov@evrone.ru>
2013-06-22 16:46:16 +0400

    Add 4th example

---
+++ abstract-factory-in-ruby/abstract_factory_4.rb
@@ -0,0 +1,67 @@
+# Abstract Factory 4
+#
+class Repository
+  attr_reader :app, :namespace
+
+  def initialize(app, type)
+    @app = app
+    @namespace = "Repositories::#{type.camelize}".constantize
+  rescue NameError
+    raise ArgumentError
+  end
+
+  def method_missing(meth_name, *args)
+    namespace.const_get("#{meth_name.camelize}Repository")
+  end
+
+  # -- families --
+  module Repositories
+
+    module InMemory
+
+      class MetaMessagesRepository < AbstractMetaMessagesRepository
+        # реализация методов абстрактного класса
+      end
+
+      class VideoStreamsRepository < AbstractVideoStreamsRepository
+        # реализация методов абстрактного класса
+      end
+
+    end
+
+    module Backend
+
+      class MetaMessagesRepository < AbstractMetaMessagesRepository
+        # реализация методов абстрактного класса
+      end
+
+      class VideoStreamsRepository < AbstractVideoStreamsRepository
+        # реализация методов абстрактного класса
+      end
+
+    end
+
+    class AbstractMetaMessagesRepository
+      def get_all
+        raise NotImplementedError
+      end
+    end
+
+    class AbstractVideoStreamsRepository
+      def get
+        raise NotImplementedError
+      end
+
+      class InMemory < VideoStreamsRepository
+        # реализация методов абстрактного класса
+      end
+
+      class Backend < VideoStreamsRepository
+        # реализация методов абстрактного класса
+      end
+
+    end
+
+  end
+
+end
