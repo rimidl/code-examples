f533726e1bfda1ae6ddb35bd379b7bcea492d514
Vladimir Strakhov <vl.strakhov@evrone.ru>
2013-12-23 15:40:46 +0400

    fix опечатки

--- notices-promotions/notice/promotions/instance_methods.rb
+++ notices-promotions/notice/promotions/instance_methods.rb
@@ -6,20 +6,6 @@ module Notice::Promotions
       raise NotImplementedError
     end
 
-
-    def self.promoted_by_vip
-      VipPromotion.promoted_notices(self)
-    end
-
-    def self.promoted_by_top
-      TopPromotion.promoted_notices(self)
-    end
-
-    def self.promoted_by_main_page
-      MPPromotion.promoted_notices(self)
-    end
-
-
     def promotions
       [
           main_page_promotion,
